e6789e4375364f5063c667dc09c6a7b19e4eac24
MutsukiOri <57990154+MutsukiOri@users.noreply.github.com>
2021-02-08 01:59:16 +0900

    twitter 表示の位置リンクを上に

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,14 +2,14 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>{{ contact }}</p>
-    <!-- <div class="twitter-widget"
+    <a class="twitter-timeline" data-width="360" data-height="730" href="https://twitter.com/EnsembleNix?ref_src=twsrc%5Etfw">Tweets by EnsembleNix</a> 
+    <div class="twitter-widget"
     style="width:400px; heght:500px;">
     <v-timeline
       :id="twitterId"
       :source-type="'profile'"
       :options="{ 'height': twitterHeight }" />
-      </div> -->
-    <a class="twitter-timeline" data-width="360" data-height="730" href="https://twitter.com/EnsembleNix?ref_src=twsrc%5Etfw">Tweets by EnsembleNix</a> 
+      </div>
   </div>
 
 </template>
@@ -20,9 +20,9 @@
 import Timeline from 'vue-tweet-embed/src/timeline'
 
 export default {
-  //   components: {
-  //   'v-timeline': Timeline,
-  // },
+    components: {
+    'v-timeline': Timeline,
+  },
   name: 'HelloWorld',
   data () {
     return {
@@ -50,6 +50,10 @@ h1 {
 }
 a {
   background-color: aquamarine;
+  transition: 1s;
+}
+a :hover {
+  background-color: gray;
 }
 </style>
 
